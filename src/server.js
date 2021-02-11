64a097f6c205c7eee729d5d542cd443d055d997c
EddieSCJ <eddiestudyifs@gmail.com>
2021-02-10 21:51:26 -0300

    Adding cors configuration

--- src/server.js
+++ src/server.js
@@ -3,14 +3,18 @@ const restifyServer = restify.createServer();
 
 const server = () => {
 
+  restifyServer.use(
+    function crossOrigin(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      return next();
+    }
+  );
+
   const start = () => {
     const PORT = 8080;
     restifyServer.listen(PORT, () => {
       console.log(`Server started at localhost:${PORT}`)
-    })
-
-    restifyServer.get("/hi", (req, res) => {
-      res.send(200, "Hello");
     })
   }
 
