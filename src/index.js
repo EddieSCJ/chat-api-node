2e1c90e3cc4e54748420a0ca8136b7a4adbcee47
EddieSCJ <eddiestudyifs@gmail.com>
2021-02-11 00:29:01 -0300

    Starting services

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const server = require('./server');
 const Kafka = require('./kafka/kafka');
+const KafkaService = require('./kafka/kafkaService');
+const MessageController = require('./controller/messageController');
 
-Kafka.consume("chat-topic");
-Kafka.send("chat-topic", "Ola, primeiro teste");
+Kafka.setup();
+KafkaService.consume('chat-topic');
+
 server.start();
+
+MessageController.startPost();
